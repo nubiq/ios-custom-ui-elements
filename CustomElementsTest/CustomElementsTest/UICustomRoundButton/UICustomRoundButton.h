c3cf28c9f34eee2c404808fdd5c7ebe5981e1b5d
Noelia Sales <noelia@bebeinnova.com>
2013-05-15 11:19:53 +0200

    Adding UICustomRoundButton class with init methods.

---
+++ CustomElementsTest/CustomElementsTest/UICustomRoundButton/UICustomRoundButton.h
@@ -0,0 +1,83 @@
+//
+//  UICustomRoundButton.h
+//  Prueba boton
+//
+//  Created by Juanma Coello on 22/01/13.
+//  Copyright (c) 2013 BebeInnova S.L. All rights reserved.
+//
+// Code in this file:
+// - Juanma Coello <juanma@nubiq.es>
+// - Noelia Sales <noelia@nubiq.es>
+
+#import <UIKit/UIKit.h>
+
+// Customized icons macros
+#define UICustomIconAdd        @"add_icon.png"
+#define UICustomIconSubstract  @"substract_icon.png"
+#define UICustomIconEdit       @"glyphicons_edit_pencil.png"
+#define UICustomIconPlay       @"glyphicons_play.png"
+#define UICustomIconRemove     @"glyphicons_remove.png"
+#define UICustomIconZoomIn     @"glyphicons_zoom_in.png"
+#define UICustomIconZoomOut    @"glyphicons_zoom_out.png"
+#define UICustomIconNext       @"glyphicons_next_chevron.png"
+#define UICustomIconPrevious   @"glyphicons_previous_chevron.png"
+
+// Customized colors macros
+#define UICustomColorBlack [UIColor colorWithRed:0.2f green:0.2f blue:0.2f alpha:1.0f]
+#define UICustomColorDarkRed [UIColor colorWithRed:0.71f green:0.01f blue:0.01f alpha:1.0f]
+#define UICustomColorRed [UIColor colorWithRed:1.0f green:0 blue:0 alpha:1.0f]
+#define UICustomColorPink [UIColor colorWithRed:1.0f green:0 blue:0 alpha:1.0f]
+#define UICustomColorTeal [UIColor colorWithRed:0 green:0.5f blue:0.5f alpha:1.0f]
+#define UICustomColorGreen [UIColor colorWithRed:0 green:0.5f blue:0 alpha:1.0f]
+#define UICustomColorBrightGreen [UIColor colorWithRed:0 green:1.0f blue:0 alpha:1.0f]
+#define UICustomColorTurquoise [UIColor colorWithRed:0 green:1.0f blue:1.0f alpha:1.0f]
+#define UICustomColorDarkBlue [UIColor colorWithRed:0 green:0 blue:0.5f alpha:1.0f]
+#define UICustomColorViolet [UIColor colorWithRed:0.5f green:0 blue:0.5f alpha:1.0f]
+#define UICustomColorBlue [UIColor colorWithRed:0 green:0 blue:1.0f alpha:1.0f]
+#define UICustomColorLightGray [UIColor colorWithRed:0.75f green:0.75f blue:0.75f alpha:1.0f]
+#define UICustomColorGray [UIColor colorWithRed:0.5f green:0.5f blue:0.5f alpha:1.0f]
+#define UICustomColorDarkYellow [UIColor colorWithRed:0.5f green:0.5f blue:0 alpha:1.0f]
+#define UICustomColorYellow [UIColor colorWithRed:1.0f green:1.0f blue:0 alpha:1.0f]
+#define UICustomColorWhite [UIColor colorWithRed:1.0f green:1.0f blue:1.0f alpha:1.0f]
+#define UICustomColorBlueIOS [UIColor colorWithRed:0.392f green:0.6f blue:0.89f alpha:1.0f]
+#define UICustomColorRedIOS [UIColor colorWithRed:0.81f green:0 blue:0 alpha:1.0f]
+
+
+/**
+ * Special class to create customized buttons in iOS style.
+ */ 
+@interface UICustomRoundButton : UIButton <UIGestureRecognizerDelegate>
+
+/**
+ * Main color of the button.
+ */
+@property (nonatomic) UIColor *color;
+
+/**
+ * Image showed in the center of the button.
+ */
+@property (nonatomic) UIImage *icon;
+
+/**
+ *
+ */
+@property BOOL selected;
+
+// METHODS
+
+/**
+ * Init button with a given frame. Icon and color are assigned by default.
+ */
+- (id)initWithFrame:(CGRect)frame;
+
+/**
+ * Init button with a given frame and icon. Color is assigned by default.
+ */
+- (id)initWithFrame:(CGRect)frame icon:(NSString *)iconName;
+
+/**
+ * Init button with a given frame, icon and color.
+ */
+- (id)initWithFrame:(CGRect)frame icon:(NSString *)iconName color:(UIColor *)color;
+
+@end
---
+++ CustomElementsTest/CustomElementsTest/UICustomRoundButton/UICustomRoundButton.m
@@ -0,0 +1,156 @@
+//
+//  UICustomRoundButton.m
+//  Prueba boton
+//
+//  Created by Juanma Coello on 22/01/13.
+//  Copyright (c) 2013 BebeInnova S.L. All rights reserved.
+//
+// Code in this file:
+// - Juanma Coello <juanma@nubiq.es>
+// - Noelia Sales <noelia@nubiq.es>
+
+#import "UICustomRoundButton.h"
+#include <math.h>
+
+@implementation UICustomRoundButton
+
+@synthesize icon;
+@synthesize selected = _selected;
+
+- (id)init
+{
+    self = [super initWithFrame:CGRectMake(0, 0, 26, 26)];
+    
+    float r = 0x58p1/0xffp1;
+    float g = 0x91p1/0xffp1;
+    float b = 0xe1p1/0xffp1;
+    float a = 1;
+    
+    [self setColor:[UIColor colorWithRed:r green:g blue:b alpha:a]];
+    [self setIcon:[UIImage imageNamed:UICustomIconRemove]];
+    
+    return self;
+}
+ 
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    [self setSelected:NO];
+    [self addTarget:self action:@selector(buttonTouched:forEvent:) forControlEvents:UIControlEventAllTouchEvents];
+    return self;
+}
+
+- (id)initWithFrame:(CGRect)frame icon:(NSString *)iconName
+{
+    self = [self initWithFrame:frame];
+    [self setIcon:[UIImage imageNamed:iconName]];
+    return self;
+}
+
+- (id)initWithFrame:(CGRect)frame icon:(NSString *)iconName color:(UIColor *)color
+{
+    self = [self initWithFrame:frame icon:iconName];
+    [self setColor:color];
+    return self;
+}
+
+- (void)drawRect:(CGRect)rect
+{    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    float avalaibleSize =
+        self.bounds.size.height < self.bounds.size.width ?
+        self.bounds.size.height :
+        self.bounds.size.width;
+    
+    // Defining rects for every layer
+    CGRect outerBorder = CGRectMake(self.bounds.origin.x, self.bounds.origin.y,
+                                    avalaibleSize-1, avalaibleSize-1);
+    CGRect outerCircle = CGRectMake(self.bounds.origin.x+0.5, self.bounds.origin.y+0.5,
+                                    avalaibleSize-3, avalaibleSize-3);
+    CGRect innerCircle = CGRectMake(self.bounds.origin.x+3, self.bounds.origin.y+3,
+                                    avalaibleSize-7, avalaibleSize-7);
+    CGRect glossCircle = CGRectMake(self.bounds.origin.x-avalaibleSize/2, self.bounds.origin.y+avalaibleSize/2,
+                                    avalaibleSize*2, avalaibleSize*1.5);
+    
+    CGFloat angle = atan(icon.size.height/icon.size.width);
+    CGRect imageCircle = CGRectMake(4 + ((avalaibleSize-8)/2) - (cos(angle)*(avalaibleSize-8)/2),
+                                    4 + ((avalaibleSize-8)/2) - (sin(angle)*(avalaibleSize-8)/2),
+                                    cos(angle)*(avalaibleSize-8), sin(angle)*(avalaibleSize-8));
+    
+    
+    CGMutablePathRef outerBorderPath = CGPathCreateMutable();
+    CGPathAddEllipseInRect(outerBorderPath, NULL, outerBorder);
+    
+    CGMutablePathRef outerCirclePath = CGPathCreateMutable();
+    CGPathAddEllipseInRect(outerCirclePath, NULL, outerCircle);
+    
+    CGMutablePathRef innerCirclePath = CGPathCreateMutable();
+    CGPathAddEllipseInRect(innerCirclePath, NULL, innerCircle);
+    
+    CGMutablePathRef glossCirclePath = CGPathCreateMutable();
+    CGPathAddEllipseInRect(glossCirclePath, NULL, glossCircle);
+    
+    CGMutablePathRef topShadowCirclePath = CGPathCreateMutable();
+    CGPathAddEllipseInRect(topShadowCirclePath, NULL, outerCircle);
+    
+    // Draw shadow
+    CGContextSaveGState(context);
+    CGContextSetShadowWithColor(context,CGSizeMake (0.5,  0.5), 1,
+                                [[UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:0.5] CGColor]);
+    CGContextSetFillColorWithColor(context, [[UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:0.5] CGColor]);
+    CGContextAddPath(context, outerBorderPath);
+    CGContextEOFillPath(context);
+    CGContextRestoreGState(context);
+    CFRelease(outerBorderPath);
+    
+    // Draw white background
+    CGContextSaveGState(context);
+    CGContextSetFillColorWithColor(context, [[UIColor whiteColor] CGColor]);
+    CGContextAddPath(context, outerCirclePath);
+    CGContextEOFillPath(context);
+    CGContextRestoreGState(context);
+    CFRelease(outerCirclePath);
+    
+    // Draw color subview
+    CGContextSaveGState(context);
+    CGContextSetFillColorWithColor(context, [[self color] CGColor]);
+    CGContextAddPath(context, innerCirclePath);
+    CGContextEOFillPath(context);
+    CGContextRestoreGState(context);
+    
+    // Draw gloss shadow
+    CGContextSaveGState(context);
+    CGContextSetBlendMode(context, kCGBlendModePlusDarker);
+    CGContextSetFillColorWithColor(context, [[UIColor colorWithRed:0.85 green:0.85 blue:0.85 alpha:1] CGColor]);
+    CGContextAddPath(context, innerCirclePath);
+    CGContextClip(context);
+    CGContextAddPath(context, glossCirclePath);
+    CGContextEOFillPath(context);
+    CGContextRestoreGState(context);
+    
+    // Draw icon in white color
+    CGContextSaveGState(context);
+    CGContextSetBlendMode(context, kCGBlendModeDestinationOut);
+    CGContextTranslateCTM(context, 0, avalaibleSize);
+    CGContextScaleCTM(context, 1, -1);
+    CGContextDrawImage(context, imageCircle, [[self icon] CGImage]);
+    CGContextDrawPath(context, kCGPathFill);
+    CGContextRestoreGState(context);
+    
+    CFRelease(innerCirclePath);
+    CFRelease(glossCirclePath);
+    
+    // Draw top shadow only when button is selected
+    if ([self selected]) {
+        CGContextSaveGState(context);
+        CGContextSetFillColorWithColor(context, [[UIColor colorWithRed:0 green:0 blue:0 alpha:0.3] CGColor]);
+        CGContextAddPath(context, topShadowCirclePath);
+        CGContextEOFillPath(context);
+        CGContextRestoreGState(context);
+        CFRelease(topShadowCirclePath);
+    }
+}
+
+@end

511e356d003af8db82750547470e10020187d6e4
Noelia Sales <noelia@bebeinnova.com>
2013-05-15 11:35:23 +0200

    Adding initialization examples.

--- CustomElementsTest/CustomElementsTest/ViewController.h
+++ CustomElementsTest/CustomElementsTest/ViewController.h
@@ -5,9 +5,17 @@
 //  Created by Noelia on 15/05/13.
 //  Copyright (c) 2013 Noelia. All rights reserved.
 //
+// Code in this file:
+// - Noelia Sales <noelia@nubiq.es>
 
 #import <UIKit/UIKit.h>
+#import "UICustomRoundButton.h"
 
 @interface ViewController : UIViewController
+
+@property (nonatomic) IBOutlet UICustomRoundButton *roundButton1;
+@property (nonatomic) IBOutlet UICustomRoundButton *roundButton2;
+@property (nonatomic) IBOutlet UICustomRoundButton *roundButton3;
+@property (nonatomic) IBOutlet UICustomRoundButton *roundButton4;
 
 @end
--- CustomElementsTest/CustomElementsTest/ViewController.m
+++ CustomElementsTest/CustomElementsTest/ViewController.m
@@ -5,6 +5,8 @@
 //  Created by Noelia on 15/05/13.
 //  Copyright (c) 2013 Noelia. All rights reserved.
 //
+// Code in this file:
+// - Noelia Sales <noelia@nubiq.es>
 
 #import "ViewController.h"
 
@@ -17,13 +19,42 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Init rounded buttons
+    _roundButton1 = [[UICustomRoundButton alloc] initWithFrame:CGRectMake(100, 100, 20, 20)
+                                                          icon:UICustomIconPrevious
+                                                         color:UICustomColorBlack];
+    _roundButton2 = [[UICustomRoundButton alloc] initWithFrame:CGRectMake(140, 100, 26, 26)
+                                                          icon:UICustomIconRemove
+                                                         color:UICustomColorTeal];
+    _roundButton3 = [[UICustomRoundButton alloc] initWithFrame:CGRectMake(180, 100, 30, 30)
+                                                          icon:UICustomIconSubstract
+                                                         color:UICustomColorRedIOS];
+    _roundButton4 = [[UICustomRoundButton alloc] initWithFrame:CGRectMake(220, 100, 26, 26)
+                                                          icon:UICustomIconAdd
+                                                         color:UICustomColorBlueIOS];
+    
+    // Set a method to launch when the button is pushed
+    [_roundButton1 addTarget:self action:@selector(roundButtonClicked:)
+            forControlEvents:UIControlEventTouchUpInside];
+    
+    // Add all buttons as subviews
+    [[self view] addSubview:_roundButton1];
+    [[self view] addSubview:_roundButton2];
+    [[self view] addSubview:_roundButton3];
+    [[self view] addSubview:_roundButton4];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Buttons
+
+- (IBAction)roundButtonClicked:(id)sender
+{
+    NSLog(@"Clicked round button");
 }
 
 @end
